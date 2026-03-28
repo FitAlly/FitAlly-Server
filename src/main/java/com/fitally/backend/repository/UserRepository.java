2ecd5cd3d102ce759b546f7de7dd1b156fac1d0e
권혁진 <gurwls980423@gmail.com>
2026-03-29 00:35:13 +0900

    복구완료

---
+++ src/main/java/com/fitally/backend/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.fitally.backend.repository;
+
+import com.fitally.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmailAndDeletedAtIsNull(String email);
+
+    boolean existsByNicknameAndDeletedAtIsNull(String nickname);
+
+    Optional<User> findByEmailAndDeletedAtIsNull(String email);
+
+    Optional<User> findByUserIdAndDeletedAtIsNull(Long userId);
+}
