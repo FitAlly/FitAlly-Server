0d9726b369330910a0f69540b999fa207d03d511
권혁진 <gurwls980423@gmail.com>
2026-03-29 00:53:49 +0900

    로그인 기능 + 테스트코드 구현 완료

--- src/main/java/com/fitally/backend/dto/auth/request/SignupRequest.java
+++ src/main/java/com/fitally/backend/dto/auth/request/SignupRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 public class SignupRequest {
 
